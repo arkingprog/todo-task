047964646fc437da6f97922d0a9ba7ec91433c5c
Artem Gordienko <arking@ukr.net>
2016-08-29 18:35:04 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 var config={
-    'secret':'sectertokenforjwt',
-    'databaseUrl':'postgres://gnxhobringpqjy:3KuSCO4nBlUZ_EYSk-v_WXzeKs@ec2-107-21-229-87.compute-1.amazonaws.com:5432/d6cs0p001bbf4j?ssl=true'
+    'databaseUrl':'<url database>'
 }
 
 module.exports=config;
