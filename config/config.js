f1835c27aeece408894aa1b87bafcc736852cfed
Artem Gordienko <arking@ukr.net>
2016-09-20 10:25:39 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 var config={
-    'databaseUrl':'<url database>'
+    'databaseUrl':'postgres://ocxitzuqvihisr:X6p-VelgjUtJG_m6igY5LIh__f@ec2-23-21-235-126.compute-1.amazonaws.com:5432/d87nh9a60mmm1r?ssl=true'
 }
 
 module.exports=config;
